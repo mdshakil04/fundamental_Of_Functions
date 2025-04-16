da956c53d03a7ad690531fa2b4a62330ff338003
mdshakil04 <m.shakil04@yahoo.com>
2025-04-16 08:04:09 +0600

    Function started

---
+++ function.js
@@ -0,0 +1,9 @@
+function school(){
+    // console.log("Wake up early in the morning")
+}
+school()
+function multification(a,b){
+    const gunon = a * b;
+    console.log(gunon)
+}
+multification(12, 5)
