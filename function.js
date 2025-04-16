24ca39a59143caca3a469865a5f0f3e0a6912710
mdshakil04 <m.shakil04@yahoo.com>
2025-04-16 08:38:51 +0600

    Some function related task done

--- function.js
+++ function.js
@@ -5,6 +5,35 @@ school()
 // Task 1
 function multification(a,b, c, d){
     const gunon = a * b * c* d;
-    console.log(gunon)
+    // console.log(gunon)
 }
 multification(12, 5, 6, 7)
+
+let num = 13
+if(num % 2 === 0 ){
+    let result = num / 2
+    // console.log(result)
+}else{
+    let result = num * 2
+    // console.log(result)
+}
+// --------------------
+let num2 = 120;
+if(num2 > 100){
+    const result = num2 - 100;
+    // console.log(result)
+}else if(num2 >= 100){
+    const result = num2 + 100;
+    // console.log(result)
+}
+// -------------------------
+function task1(num){
+    if(num % 2 === 0 ){
+        return result = num / 2
+        // console.log(result)
+    }else{
+        return result = num * 2
+        // console.log(result)
+    }
+}
+console.log(task1(61))
