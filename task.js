979a0930b3690d1911ecb434b2cb58d11cdad727
mdshakil04 <m.shakil04@yahoo.com>
2025-04-21 12:16:27 +0600

    another task done

--- task.js
+++ task.js
@@ -6,5 +6,16 @@ function multifly(a,b,c,d){
 function multiflyAll(...numbers){
     return numbers.reduce((total, num) => total * num, 1);
 }
-const result = multiflyAll(2,3,4,5);
-console.log(result);
+// const result = multiflyAll(2,3,4,5);
+// console.log(result);
+
+function playWithNumbers(num){
+    if(num %2 === 1 ){
+        return num*2;
+    }
+    else{
+        return num / 2;
+    }
+}
+const result = playWithNumbers(120);
+console.log(result)
