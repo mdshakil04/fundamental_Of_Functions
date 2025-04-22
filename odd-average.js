8f4a2e6cdf87c731d83c1cce6433f3c3a0e93cf9
mdshakil04 <m.shakil04@yahoo.com>
2025-04-22 08:26:22 +0600

    Task is complited

--- odd-average.js
+++ odd-average.js
@@ -1,16 +1,25 @@
 function oddAverage(numbers){
-    // console.log(numbers)
-    let sum = 0;
-
+    const oddsArray = [];
     for(const number of numbers){
-        
         if(number %2 === 1){
-             sum += number
-            //  console.log(sum)
-             let avg = sum / 5;
+            // console.log(number)
+            oddsArray.push(number)
             
         } 
     }
+    // ------------For average of oddsArray-------------
+    let sum = 0;
+    for (const number of oddsArray){
+       sum += number
+    }
+    
+    const count = oddsArray.length
+    const avg = sum / count;
+    // console.log(avg)
+    return avg
+
+
+    // console.log(oddsArray)
     // for(let i = 0; i <= numbers.length; i++){
     //     console.log(i)
     // }
