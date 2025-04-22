7322ad00d30f5978b0575b6d550204410ea4d1a5
mdshakil04 <m.shakil04@yahoo.com>
2025-04-22 07:44:11 +0600

    My code

--- odd-average.js
+++ odd-average.js
@@ -1,5 +1,19 @@
 function oddAverage(numbers){
-    console.log(numbers)
+    // console.log(numbers)
+    let sum = 0;
+
+    for(const number of numbers){
+        
+        if(number %2 === 1){
+             sum += number
+            //  console.log(sum)
+             let avg = sum / 5;
+            
+        } 
+    }
+    // for(let i = 0; i <= numbers.length; i++){
+    //     console.log(i)
+    // }
 }
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const avg = oddAverage(numbers)
