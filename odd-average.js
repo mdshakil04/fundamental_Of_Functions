518c747f2ec319d242b8f75f678a0a129a0cde0d
mdshakil04 <m.shakil04@yahoo.com>
2025-04-22 07:29:22 +0600

    New task started

---
+++ odd-average.js
@@ -0,0 +1,6 @@
+function oddAverage(numbers){
+    console.log(numbers)
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const avg = oddAverage(numbers)
+console.log("Average of odd numbers in given array is", avg)
