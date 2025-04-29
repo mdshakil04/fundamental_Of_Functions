0b64fd2e3ace4449f6d11bbd2d9cce7bc0586009
mdshakil04 <m.shakil04@yahoo.com>
2025-04-29 19:05:26 +0600

    Returning

--- function-conceptual.js
+++ function-conceptual.js
@@ -2,3 +2,9 @@ function printName(name, age, money){
     console.log(`my Name is ${name} , my age is ${age}, I have ${money} taka`)
 }
 printName("Shakil", 25, 23454560)
+function add(x,y){
+    const total = x + y;
+    return total;
+}
+const result = add(34, 54);
+console.log(result)
