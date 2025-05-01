91c8bfd7f1be101bdfa5b338d698b457a6a70afe
mdshakil04 <m.shakil04@yahoo.com>
2025-05-01 23:25:42 +0600

    Assignment task 1 done

--- assignment-task.js
+++ assignment-task.js
@@ -1,6 +1,21 @@
-let income = 40000;
-let expense = 32000;
+const income = 50000;
+const expense = 10000;
 function calculateTax(i, e){
-    console.log(i,e)
+    
+    if(i < 0 || e < 0){
+        console.log("Please! enter positive number")
+    }
+    else if(typeof i !== "number" || typeof e !== "number" ){
+        console.log("You have to enter number")
+    }
+    else if(i < e){
+        console.log("Cut your coat according to your cloth")
+    }
+    else{
+        const savings = i - e;
+        const tax = savings * 0.2
+        return tax
+    }
 }
 const result = calculateTax(income, expense)
+console.log(result)
